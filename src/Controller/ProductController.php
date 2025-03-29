f187bd03f0afd4c09b3bd055d08543ee78e26780
Tayss1r <ferhitayssir@gmail.com>
2025-03-29 12:10:49 +0100

    fixed categories

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,27 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Enum\animalType;
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[
-    Route('/product'),
-]
 final class ProductController extends AbstractController
 {
-    #[Route('/all', name: 'app_findAll_products')]
-    public function menu(CategoryRepository $categoryRepository): Response
+    #[Route('/home', name: 'app_home')]
+    public function sidebar(ManagerRegistry $doctrine): Response
     {
-        // Fetch all categories from the database
-        $categories = $categoryRepository->findAll();
+        $repo = $doctrine->getRepository(Category::class);
+        $categories = $repo->findAll();
         $animalType = animalType::cases();
 
-        return $this->render('/product/findAll.html.twig', [
+        return $this->render('home.html.twig', [
             'categories' => $categories,
             'animalType' => $animalType,
         ]);
     }
+
+    #[Route('/{animalType}/{categoryName}', name: 'app_category')]
+    public function findProduct(
+        string $animalType,
+        string $categoryName,
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository
+    ): Response {
+        // Fetch the products based on animalType and categoryName
+        $products = $productRepository->findByCategory($animalType, $categoryName);
+
+        // Fetch all categories if you want to display them in the menu
+        $categories = $categoryRepository->findAll();
+
+        // You can also fetch animal types if needed
+        $animalType = animalType::cases();
+
+        // Render the page, passing the relevant data to the template
+        return $this->render('category/category.html.twig', [
+            'products' => $products,
+            'categories' => $categories,
+            'animalType' => $animalType,
+        ]);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,17 +19,17 @@ class ProductRepository extends ServiceEntityRepository
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByCategory($animalType, $categoryName): array
+    {
+        return $this->createQueryBuilder('p')
+            ->join('p.category', 'c')  // Assuming there's a 'category' relation in Product entity
+            ->andWhere('p.type = :animalType')
+            ->andWhere('c.name = :categoryName')  // Assuming 'name' is the category field in the Category entity
+            ->setParameter('animalType', $animalType)
+            ->setParameter('categoryName', $categoryName)
+            ->getQuery()
+            ->getResult();
+    }
 
 //    public function findOneBySomeField($value): ?Product
 //    {
